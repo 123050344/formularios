fb19786bddb956f2202a464812f259707bd8ab99
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-03-14 22:06:20 -0600

    Agregar archivos de proyecto.

---
+++ formularios/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace formularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ListViewItem listViewItem2 = new ListViewItem("Hola");
+            errorProvider1 = new ErrorProvider(components);
+            checkBox1 = new CheckBox();
+            bindingSource1 = new BindingSource(components);
+            button1 = new Button();
+            label1 = new Label();
+            checkBox2 = new CheckBox();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            imageList1 = new ImageList(components);
+            notifyIcon1 = new NotifyIcon(components);
+            colorDialog1 = new ColorDialog();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            pictureBox1 = new PictureBox();
+            numericUpDown1 = new NumericUpDown();
+            radioButton1 = new RadioButton();
+            label3 = new Label();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            listBox1 = new ListBox();
+            listView1 = new ListView();
+            toolTip1 = new ToolTip(components);
+            comboBox1 = new ComboBox();
+            monthCalendar1 = new MonthCalendar();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox1.Location = new Point(276, 76);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(67, 36);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Yes";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 0, 0);
+            button1.Font = new Font("Forte", 28F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(25, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 83);
+            button1.TabIndex = 1;
+            button1.Text = "puca";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe Script", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(246, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(293, 44);
+            label1.TabIndex = 2;
+            label1.Text = "¿Do you like puca?";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox2.Location = new Point(423, 76);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(67, 36);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "Yes";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CalendarMonthBackground = Color.Red;
+            dateTimePicker1.CalendarTitleBackColor = Color.Red;
+            dateTimePicker1.CalendarTrailingForeColor = SystemColors.Control;
+            dateTimePicker1.Font = new Font("Forte", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            dateTimePicker1.Location = new Point(246, 171);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(371, 35);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe Script", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(246, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(349, 44);
+            label2.TabIndex = 5;
+            label2.Text = "¿When puca was born?";
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "RobloxScreenShot20231201_170208163.png");
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = Properties.Resources.puca;
+            pictureBox1.Location = new Point(27, 142);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(204, 161);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.BackColor = Color.Red;
+            numericUpDown1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            numericUpDown1.Location = new Point(246, 226);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(177, 25);
+            numericUpDown1.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(246, 314);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(50, 19);
+            radioButton1.TabIndex = 9;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Garu";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe Script", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(246, 267);
+            label3.Name = "label3";
+            label3.Size = new Size(354, 44);
+            label3.TabIndex = 10;
+            label3.Text = "¿Who is her boyfriend?";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(246, 339);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(73, 19);
+            radioButton2.TabIndex = 11;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "iron man";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(246, 364);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(53, 19);
+            radioButton3.TabIndex = 12;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "shrek";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(255, 192, 192);
+            listBox1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 28;
+            listBox1.Items.AddRange(new object[] { "hola ", "mi name es pucca", "i like apples" });
+            listBox1.Location = new Point(691, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(173, 88);
+            listBox1.TabIndex = 13;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // listView1
+            // 
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem2 });
+            listView1.Location = new Point(691, 115);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(177, 98);
+            listView1.TabIndex = 14;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged_1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "testing                                                                                 ", "testing one", "testing two" });
+            comboBox1.Location = new Point(659, 252);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(187, 23);
+            comboBox1.TabIndex = 15;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.BackColor = Color.Firebrick;
+            monthCalendar1.Location = new Point(637, 286);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Maroon;
+            ClientSize = new Size(903, 466);
+            Controls.Add(monthCalendar1);
+            Controls.Add(comboBox1);
+            Controls.Add(listView1);
+            Controls.Add(listBox1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(label3);
+            Controls.Add(radioButton1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(checkBox2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ErrorProvider errorProvider1;
+        private CheckBox checkBox1;
+        private BindingSource bindingSource1;
+        private Button button1;
+        private CheckBox checkBox2;
+        private Label label1;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private ImageList imageList1;
+        private NotifyIcon notifyIcon1;
+        private ColorDialog colorDialog1;
+        private ContextMenuStrip contextMenuStrip1;
+        private PictureBox pictureBox1;
+        private NumericUpDown numericUpDown1;
+        private RadioButton radioButton1;
+        private Label label3;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private ListBox listBox1;
+        private ListView listView1;
+        private ComboBox comboBox1;
+        private ToolTip toolTip1;
+        private MonthCalendar monthCalendar1;
+    }
+}
---
+++ formularios/Form1.cs
@@ -0,0 +1,30 @@
+namespace formularios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
